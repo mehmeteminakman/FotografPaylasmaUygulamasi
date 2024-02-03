17e3d031fddd628982ddec16a7075759bae6c2a5
mehmeteminakman <akmanmehmetemin031@gmail.com>
2024-02-03 16:48:27 +0300

    Firestore a kaydedilmiş email, görsel ve yorumları çekip feed kısmında gösterdik. Görsel çekmek için SDWebImage paketini kullandık.

--- FotografPaylasmaUygulamasi/FeedViewController.swift
+++ FotografPaylasmaUygulamasi/FeedViewController.swift
@@ -6,26 +6,88 @@
 //
 
 import UIKit
+import Firebase
+import SDWebImage
 
 class FeedViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
-    
+    var emailDizisi = [String]()
+    var yorumDizisi = [String]()
+    var gorselDizisi = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        
     }
+    override func viewWillAppear(_ animated: Bool) {
+        firebaseVerileriAl()
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return emailDizisi.count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "feedCell", for: indexPath) as! feedCell
-        cell.emailLabel.text = "akman@gmail.com"
-        cell.yorumLabel.text = "test yorum.ebhckbeckhbekhbchksbcksbckhjsbcksbcksjbckjsbcjs"
-        cell.postImageView.image = UIImage(named: "manzara")
+        cell.emailLabel.text = emailDizisi[indexPath.row]
+        cell.yorumLabel.text = yorumDizisi[indexPath.row]
+        cell.postImageView.sd_setImage(with: URL(string: gorselDizisi[indexPath.row]))
         return cell
+    }
+    
+    
+    
+    func firebaseVerileriAl(){
+        
+        
+        emailDizisi.removeAll()
+        yorumDizisi.removeAll()
+        gorselDizisi.removeAll()
+        let firebase = Firestore.firestore()
+        
+        firebase.collection("Post").addSnapshotListener { querysnapshot, error in
+            if error != nil{
+                self.mesajGoster(title: "Hata !", message: error!.localizedDescription)
+            }else{
+                if querysnapshot?.isEmpty != true && querysnapshot != nil {
+                    for document in querysnapshot!.documents {
+                        
+                        if let gorsel = document.get("image") as? String{
+                            self.gorselDizisi.append(gorsel)
+                        }
+                        if let email = document.get("email") as? String{
+                            self.emailDizisi.append(email)
+                        }
+                        if let yorum = document.get("yorum") as? String{
+                            self.yorumDizisi.append(yorum)
+                        }
+                        
+                        
+                    }
+                    self.tableView.reloadData()
+                    
+                }
+                
+            }
+            
+            
+            
+            
+            
+        }
+        
+    }
+    
+    
+    
+    
+    func mesajGoster(title :String,message :String){
+        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        self.present(alert, animated: true, completion: nil)
     }
 
    
